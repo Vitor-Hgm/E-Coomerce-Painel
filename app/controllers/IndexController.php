302cde32e70c98a310fa1d12bee85193c1655ef1
Vitor-Hgm <mvitorhugo862@gmail.com>
2025-11-03 15:00:57 -0300

    Criando os primeiros arquivos da pasta controllers, models e config

---
+++ app/controllers/IndexController.php
@@ -0,0 +1,7 @@
+<?php
+    require "../../config/Conexao.php";
+    require "../models/Usuario.php";
+
+    class IndexController{
+        
+    }
---
+++ public/index.php
@@ -0,0 +1,34 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <base href="http://<?=$_SERVER["SERVER_NAME"].$_SERVER["SCRIPT_NAME"]?>">
+    <title>Painel E-coomerce</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/sweetalert2.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    
+    <!-- Summer Note -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.9.0/dist/summernote.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.9.0/dist/summernote.min.js"></script>
+
+</head>
+<body>
+    <?php       
+    
+    if (($_POST) && (!isset($_SESSION["usuario"]))) {
+
+        require "../app/controllers/IndexController.php";
+    }
+
+    ?>
+</body>
+</html>
